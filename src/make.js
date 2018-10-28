d76b37c43301fe54b70d1012dd037a5a1c488247
anaumovich <36381463+anaumovich@users.noreply.github.com>
2018-10-28 20:11:15 +0300

    refactor: make.js

--- src/make.js
+++ src/make.js
@@ -1,2 +1,16 @@
+const sum = (a, b) => a + b;
+function make(...a) {
+  let array = a;
+  let result = 0;
 
+  function x(c, ...b) {
+    if (typeof c !== 'number') {
+      result = array.reduce(sum);
+      return result;
+    }
+    array = array.concat(c, ...b);
+    return x;
+  }
+  return x;
+}
 // console.log(make(15)(34, 21, 666)(41)(sum));
